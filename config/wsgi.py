b4c479e1c7a8a984f09475cbf1b0f06dba149de1
Chibuzor <nwalorc@gmail.com>
2017-11-21 01:11:17 +0100

    Added whitenoise to requirements

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,17 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
-import os
-import sys
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-print(sys.path)
-
 from config.environment import SETTINGS_MODULE
 from whitenoise.django import DjangoWhiteNoise
-
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
 application = get_wsgi_application()
